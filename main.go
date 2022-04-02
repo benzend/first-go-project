ad1a82688854f3aa751f66be3b8bb9e16e53bd06
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-04-02 04:53:41 -0600

    chore: renamed number-gen to character-gen

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ type App struct {
 	fn func()
 }
 
-var apps = []App{{name: "tickets", fn: applications.TicketApp}, {name: "number-gen", fn: applications.CharacterGenApp}}
+var apps = []App{{name: "tickets", fn: applications.TicketApp}, {name: "character-gen", fn: applications.CharacterGenApp}}
 
 func main() {
 	app := getChosenApp()
